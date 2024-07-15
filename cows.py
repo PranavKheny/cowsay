8502ff9a9153e39b03c4ee3ce978c7901de8ac7d
PranavKheny <pranavkheny@gmail.com>
2024-07-15 11:01:18 -0400

    first commit

---
+++ cows.py
@@ -0,0 +1,11 @@
+import cowsay
+
+# Print a message with the default cow
+print("hello world")
+cowsay.dragon("Hello, Pranav!")
+
+# List all available characters
+print(cowsay.char_names)
+
+# Print a message with a different character
+cowsay.tux("Welcome to the aid310 environment!")
